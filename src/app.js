da4a4f90c1880a0858990bd3ac011abc27303e09
GabrielCastro1221 <gbrlcstr@hotmail.com>
2024-10-09 21:14:34 -0500

    .

--- src/app.js
+++ src/app.js
@@ -9,21 +9,30 @@ const routerKeys = require("./routes/env.routes");
 const routerProjects = require("./routes/projects.routes");
 
 const app = express();
+const mode = process.argv[2] || "dev"; // Argumento de línea de comando para definir el modo
 const port = configObject.server.port;
 
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOpt));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(cors(corsOptions));
 
 app.use("/api/v1/keys", routerKeys);
 app.use("/api/v1/projects", routerProjects);
 
+if (mode === "--mode build") {
+  app.use(express.static(path.join(__dirname, "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
+
 app.listen(port, () => {
   try {
     logger.info(
-      `Server listening on port ${port} and ruuning on URL http://localhost:${port}`
+      `Server running in ${mode === "--mode build" ? "production" : "development"} mode on port ${port}`
     );
+    logger.info(`Server URL: http://localhost:${port}`);
   } catch (err) {
     logger.error("Internal server error", err.message);
   }
--- src/middlewares/cors.middleware.js
+++ src/middlewares/cors.middleware.js
@@ -1,7 +1,5 @@
-const configObject = require("../config/env.config");
-
 const corsOptions = {
-  origin: configObject.server.cors_origin || "*",
+  origin: mode === "--mode build" ? configObject.server.cors_origin : "http://localhost:3000",
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
