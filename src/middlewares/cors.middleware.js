ddf210db7cbb19e3be6a4b475097a5e017b55bed
GabrielCastro1221 <gbrlcstr@hotmail.com>
2024-10-09 20:40:38 -0500

    .

--- src/middlewares/cors.middleware.js
+++ src/middlewares/cors.middleware.js
@@ -1,7 +1,7 @@
 const configObject = require("../config/env.config");
 
 const corsOptions = {
-  origin: configObject.server.cors_origin,
+  origin: configObject.server.cors_origin || "*",
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
